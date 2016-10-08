f35ee234896e5e97b2815a1920b16729817f9a83
陈波 <chenbotome@163.com>
2016-10-08 15:44:15 +0800

    ignore

--- controllers/apic.php
+++ controllers/apic.php
@@ -17,10 +17,10 @@ class Apic extends IController
 {
 //    public $layout='site_mini';
 
-	function init()
-	{
+    function init()
+    {
 
-	}
+    }
 
     /**
      *获取商品分类数据
@@ -56,6 +56,67 @@ class Apic extends IController
         $data = array($this->catId, $this->catRow, $this->childId);
         header("Content-type: application/json");
         echo json_encode($data);
+        exit();
+    }
+    //获取热卖商品
+    public function getCommendHot()
+    {
+        $data = Api::run('getCommendHot',8);
+        header("Content-type: application/json");
+        echo json_encode($data);
+        exit();
+    }
+    //获取品牌
+    public function getBrandList()
+    {
+        $data = Api::run('getBrandList');
+        header("Content-type: application/json");
+        echo json_encode($data);
+        exit();
+    }
+
+    //获取分类下的分类数据
+    public function getCategoryByParentid()
+    {
+        $first_id = IFilter::act(IReq::get('id'),'int');
+        $data = Api::run('getCategoryByParentid',array('#parent_id#',$first_id));
+        header("Content-type: application/json");
+        echo json_encode($data);
+        exit();
+    }
+    //获取分类下的商品
+    public function getCategoryExtendList()
+    {
+        $first_id = IFilter::act(IReq::get('id'),'int');
+        $data = Api::run('getCategoryExtendList',array('#categroy_id#',$first_id),8);
+        header("Content-type: application/json");
+        echo json_encode($data);
+        exit();
+    }
+
+    //购物车页面及商品价格计算
+    public function cart()
+    {
+        //开始计算购物车中的商品价格
+        $countObj = new CountSum();
+        $result   = $countObj->cart_count();
+
+        if(is_string($result))
+        {
+            IError::show($result,403);
+        }
+
+        //返回值
+//        $this->final_sum = $result['final_sum'];
+//        $this->promotion = $result['promotion'];
+//        $this->proReduce = $result['proReduce'];
+//        $this->sum       = $result['sum'];
+//        $this->goodsList = $result['goodsList'];
+//        $this->count     = $result['count'];
+//        $this->reduce    = $result['reduce'];
+//        $this->weight    = $result['weight'];
+        header("Content-type: application/json");
+        echo json_encode($result);
         exit();
     }
 }
