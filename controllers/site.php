1bbc7a440b8ffff23acca8d3cf4dbd914437cf2f
陈波 <chenbotome@163.com>
2016-12-05 16:31:55 +0800

    bug

--- controllers/site.php
+++ controllers/site.php
@@ -1123,7 +1123,7 @@ class Site extends IController
     }
     function recommender_login(){
         $recommender = ISession::get('recommender');
-        if (empty($recommender)){$this-$this->redirect('recommender_shop');}
+        if (!empty($recommender)){$this-$this->redirect('recommender_shop');}
         $name = IFilter::act(IReq::get('name'),'string');
         $password = IFilter::act(IReq::get('password'),'string');
         if ($name){
