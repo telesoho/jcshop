b9311691bbe5917107a44d92aaaf8bad2b638466
xiashuang <550373770@qq.com>
2016-12-10 21:39:39 +0800

    site_config修改

--- config/site_config.php
+++ config/site_config.php
@@ -1,53 +1,46 @@
-<?php
-return array (
-	'email_type' => '1',
-	'email_safe' => '',
-	'mail_address' => '',
-	'smtp' => '',
-	'smtp_user' => '',
-	'smtp_pwd' => '',
-	'smtp_port' => '',
-	'order_by' => 'new',
-	'name' => '九猫家',
-	'url' => 'http://www.jiumaojia.com',
-	'master' => '',
-	'qq' => '',
-	'email' => '',
-	'mobile' => '',
-	'phone' => '',
-	'address' => '',
-	'site_footer_code' => '<div class=\\"footer\\" style=\\"text-align:center;\\">	<p class=\\"links\\">		<a href=\\"\\">关于我们</a>|<a href=\\"\\">常见问题</a>|<a href=\\"\\">安全交易</a>|<a href=\\"\\">购买流程</a>|<a href=\\"\\">如何付款</a>|<a href=\\"\\">联系我们</a>|<a href=\\"\\">合作提案</a>	</p>	<p class=\\"copyright\\">		Powered by 九猫家	</p>Copyright © 2005-2014 <a class=\\"copyys\\" target=\\"_blank\\" href=\\"http://www.miibeian.gov.cn/\\">鲁ICP备01000010号</a></div>',
-	'tax' => '10',
-	'stockup_time' => '1',
-	'index_seo_title' => '九猫家',
-	'index_seo_keywords' => '九猫家',
-	'index_seo_description' => '微商城',
-	'goods_no_pre' => 'SD',
-	'commission' => '',
-	'store_num_warning' => '1',
-	'reg_option' => '0',
-	'index_slide' => 'a:5:{i:0;a:3:{s:4:"name";s:6:"正品";s:3:"url";s:24:"http://www.jiumaojia.com";s:3:"img";s:39:"upload/2016/11/04/20161104093831282.jpg";}i:1;a:3:{s:4:"name";s:12:"万种商品";s:3:"url";s:24:"http://www.jiumaojia.com";s:3:"img";s:39:"upload/2016/11/04/20161104093831882.jpg";}i:2;a:3:{s:4:"name";s:12:"日本直邮";s:3:"url";s:24:"http://www.jiumaojia.com";s:3:"img";s:39:"upload/2016/11/04/20161104093831212.jpg";}i:3;a:3:{s:4:"name";s:12:"挑到花眼";s:3:"url";s:24:"http://www.jiumaojia.com";s:3:"img";s:39:"upload/2016/11/04/20161104093831873.jpg";}i:4;a:3:{s:4:"name";s:12:"九猫卧底";s:3:"url";s:24:"http://www.jiumaojia.com";s:3:"img";s:39:"upload/2016/11/04/20161104093831939.jpg";}}',
-	'logo' => 'upload/2016/11/04/20161104104034474.png',
-	'guide_name' => array (
-		0 => '产品官网',
-		1 => '后台管理',
-		2 => '商家管理',
-	),
-	'guide_link' => array (
-		0 => 'http://www.jiumaojia.com',
-		1 => 'systemadmin/index',
-		2 => 'seller/index',
-	),
-	'exchange_rate_jp' => '15.26', //四位
-	'score_rate' => '100', //积分和金额兑换比例
-	'score_goods_rate' => '0.07', //商品返金额比例
-	//活动ID
-	'activity_id' 	=> 1,
-	
-		
-		
-		
-)
-
-
-?>
+<?php return array (
+  'email_type' => '1',
+  'email_safe' => '',
+  'mail_address' => '',
+  'smtp' => '',
+  'smtp_user' => '',
+  'smtp_pwd' => '',
+  'smtp_port' => '',
+  'order_by' => 'new',
+  'name' => '九猫家',
+  'url' => 'http://m.jiumaojia.com',
+  'master' => '',
+  'qq' => '',
+  'email' => '',
+  'mobile' => '',
+  'phone' => '',
+  'address' => '',
+  'site_footer_code' => '<div class=\\"footer\\" style=\\"text-align:center;\\">	<p class=\\"links\\">		<a href=\\"\\">关于我们</a>|<a href=\\"\\">常见问题</a>|<a href=\\"\\">安全交易</a>|<a href=\\"\\">购买流程</a>|<a href=\\"\\">如何付款</a>|<a href=\\"\\">联系我们</a>|<a href=\\"\\">合作提案</a>	</p>	<p class=\\"copyright\\">		Powered by 九猫家	</p>Copyright © 2005-2014 <a class=\\"copyys\\" target=\\"_blank\\" href=\\"http://www.miibeian.gov.cn/\\">鲁ICP备01000010号</a></div>',
+  'tax' => '10',
+  'stockup_time' => '1',
+  'index_seo_title' => '九猫家',
+  'index_seo_keywords' => '九猫家',
+  'index_seo_description' => '微商城',
+  'goods_no_pre' => 'SD',
+  'commission' => '',
+  'store_num_warning' => '1',
+  'reg_option' => '0',
+  'index_slide' => 'a:5:{i:0;a:3:{s:4:"name";s:6:"正品";s:3:"url";s:22:"http://m.jiumaojia.com";s:3:"img";s:39:"upload/2016/11/04/20161104093831282.jpg";}i:1;a:3:{s:4:"name";s:12:"万种商品";s:3:"url";s:22:"http://m.jiumaojia.com";s:3:"img";s:39:"upload/2016/11/04/20161104093831882.jpg";}i:2;a:3:{s:4:"name";s:12:"日本直邮";s:3:"url";s:22:"http://m.jiumaojia.com";s:3:"img";s:39:"upload/2016/11/04/20161104093831212.jpg";}i:3;a:3:{s:4:"name";s:12:"挑到花眼";s:3:"url";s:22:"http://m.jiumaojia.com";s:3:"img";s:39:"upload/2016/11/04/20161104093831873.jpg";}i:4;a:3:{s:4:"name";s:12:"九猫卧底";s:3:"url";s:22:"http://m.jiumaojia.com";s:3:"img";s:39:"upload/2016/11/04/20161104093831939.jpg";}}',
+  'logo' => 'upload/2016/11/04/20161104104034474.png',
+  'guide_name' => 
+  array (
+    0 => '产品官网',
+    1 => '后台管理',
+    2 => '商家管理',
+  ),
+  'guide_link' => 
+  array (
+    0 => 'http://www.jiumaojia.com',
+    1 => 'systemadmin/index',
+    2 => 'seller/index',
+  ),
+  'exchange_rate_jp' => '15.26',
+  'score_rate' => '100',
+  'score_goods_rate' => '0.07',
+  'activity_id' => 1,
+)?>
