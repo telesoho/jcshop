9bee077c27ad6bc453852b94902f1943265389e1
wangfeng <wangfeng@qq.com>
2017-02-06 10:49:57 +0800

    完成搜索页面和分类二级子页面的加载动画以及限时购静态页面的一些修改

--- views/mobile/components/loadding.js
+++ views/mobile/components/loadding.js
@@ -1,10 +1,28 @@
 /**
  * Created by yb on 2017/1/22.
  */
-Vue.component('cat-refresh', {
-    template: '<div class="catContainer flexbox" v-if="!mes">\
-    <img src="/views/mobile/skin/default/image/jmj/icon/cat_car.png" alt="" class="ball" id="cball" />\
-    <div class="shadow"></div>\
+Vue.component('loadding', {
+    template: '<div id="loading" v-if="mes">\
+    <div class="spinner">\
+    <div class="spinner-container container1">\
+    <div class="circle1"></div>\
+    <div class="circle2"></div>\
+    <div class="circle3"></div>\
+    <div class="circle4"></div>\
+    </div>\
+    <div class="spinner-container container2">\
+    <div class="circle1"></div>\
+    <div class="circle2"></div>\
+    <div class="circle3"></div>\
+    <div class="circle4"></div>\
+    </div>\
+    <div class="spinner-container container3">\
+    <div class="circle1"></div>\
+    <div class="circle2"></div>\
+    <div class="circle3"></div>\
+    <div class="circle4"></div>\
+    </div>\
+    </div>\
     </div>',
     props:['mes']
 });
