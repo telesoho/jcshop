949e6b682f4d4936ec4becd89f0c6719cf70d569
liudawei <269035138@qq.com>
2017-02-08 09:58:30 +0800

    New changelist

--- views/mobile/javascript/jiumao/siteIndex.js
+++ views/mobile/javascript/jiumao/siteIndex.js
@@ -358,11 +358,15 @@ function index_home(self){
 //定时器  限时购
 function time_xian(){
 	times = setInterval(function(){
-		if( all_time1<0 ){
-			clearInterval(times);
-		}
+		
 		var myDate = new Date();
 		var data_time = myDate.getTime();
+		console.log()
+		if( _time-parseInt(data_time/1000)<0 ){
+			clearInterval(times);
+			shop_time = false;
+		}
+		
 		var all_time1 = _time-parseInt(data_time/1000);
 		if(all_time1<=0){
 			shop_time = false;
