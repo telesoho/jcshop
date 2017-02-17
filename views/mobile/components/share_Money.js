e51edeade57b4400d5c36ea8ee3cdaee980c6f4b
liudawei <269035138@qq.com>
2017-02-17 16:49:38 +0800

    New changelist

--- views/mobile/components/share_Money.js
+++ views/mobile/components/share_Money.js
@@ -1,3 +1,13 @@
+function active(classname){
+	var date = "?v="+Date.parse(new Date());
+	var head = document.getElementsByTagName("head")[0];
+	var share_file = document.createElement("link");
+	share_file.setAttribute("rel","stylesheet");
+	share_file.setAttribute("type","text/css");
+	share_file.setAttribute("href",classname+date);
+	head.appendChild(share_file);
+}
+active("/views/mobile/componentsCss/share_Money_component.css")
 Vue.component("sharemoney",{
 	template:'<header id="header" @click="share_detail(share.key)">\
 			   <div class="img_top">\
