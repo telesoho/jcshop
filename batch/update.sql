f1d48d83cde1c63cd5a7ce0ee3600d4dc5e2d00a
telesoho <telesoho@gmail.com>
2018-06-20 16:24:37 +0900

    修改更新数据库的SQL错误

--- batch/update.sql
+++ batch/update.sql
@@ -2,5 +2,5 @@
 ALTER TABLE `iwebshop_user` ADD COLUMN `seller_id` INT(11) NULL COMMENT '所属卖家ID' AFTER `sfz_image2`;
 -- 2018-6-20 15:15:00
 ALTER TABLE `iwebshop_user` 
-ADD COLUMN `create_time` DATETIME DEFAULT CURRENT_TIMESTAMP AFTER `seller_id` COMMENT '创建时间',
-ADD COLUMN `update_time` DATETIME ON UPDATE CURRENT_TIMESTAMP AFTER `create_time` COMMENT '更新时间';
+ADD COLUMN `create_time` DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间' AFTER `seller_id` ,
+ADD COLUMN `update_time` DATETIME ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间' AFTER `create_time` ;
